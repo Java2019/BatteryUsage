7232c2da59f6c48dd753974b9fa3cb070483bf0c
java2019 <java_2019@mail.ru>
2017-07-05 17:36:39 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/hardware/batteryusage/BatteryUsageActivity.java
@@ -0,0 +1,13 @@
+package com.samples.hardware.batteryusage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BatteryUsageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battery_usage);
+    }
+}

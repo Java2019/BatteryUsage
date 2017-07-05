427b0d12b9224bb3774710843f2b359841220744
java2019 <java_2019@mail.ru>
2017-07-05 17:38:15 +0300

    test1

--- app/src/main/java/com/samples/hardware/batteryusage/BatteryUsageActivity.java
+++ app/src/main/java/com/samples/hardware/batteryusage/BatteryUsageActivity.java
@@ -1,13 +1,23 @@
 package com.samples.hardware.batteryusage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class BatteryUsageActivity extends AppCompatActivity {
+public class BatteryUsageActivity extends AppCompatActivity
+        implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battery_usage);
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Открываем стандартный Activity с информацией о батарее
+        Intent intent = new Intent(Intent.ACTION_POWER_USAGE_SUMMARY);
+        startActivity(intent);
     }
 }
